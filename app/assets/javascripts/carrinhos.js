25d022b6ab73ecdd19191485ce070d53f0c92ede
Lucas Lise <lucas_lise1@hotmail.com>
2017-03-10 16:21:11 -0300

    criação de função

--- app/assets/javascripts/carrinhos.js
+++ app/assets/javascripts/carrinhos.js
@@ -1,7 +1,7 @@
-$(document).on("turbolinks:load", function(){
-  $(document).on("change", ".quantidade", function(){
-    var quantidade_atual = $(this).val();
-    var id_item = $(this).closest('tr').find('td[data-item]').data('item');
-    $('#carrinho').load('/carrinhos?quantidade_atual=' + quantidade_atual + '&id_item=' + id_item +' #carrinho');
-  });
+$(document).on("turbolinks:load", function() {
+    $(document).on("change", ".quantidade", function() {
+        var quantidade_atual = $(this).val();
+        var id_item = $(this).closest('tr').find('td[data-item]').data('item');
+        $('#carrinho').load('/carrinhos?quantidade_atual=' + quantidade_atual + '&id_item=' + id_item + ' #carrinho');
+    });
 });
